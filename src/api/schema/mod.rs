6961bb137f6f2fbed9f79f52664c49d18444a89d
371tti <109227273+371tti@users.noreply.github.com>
2025-10-12 14:47:12 +0900

    fix: 不要な依存関係を削除し、Tokioの機能を簡素化しました

--- src/api/schema/mod.rs
+++ src/api/schema/mod.rs
@@ -1,13 +1,2 @@
 pub mod auth;
 pub mod search;
-
-use std::ops::Range;
-
-use chrono::{DateTime, Utc};
-use serde::{Deserialize, Serialize};
-
-use crate::user_manager::auth_manager::{AccountID, AccountSession};
-
-
-
-// --- Search API schema ---
